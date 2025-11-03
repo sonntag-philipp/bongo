eb70b68a98151c85ec2e37004bf3144959121962
Flypsi <github@flyp.si>
2025-11-03 18:28:36 +0100

    feat: update supabase client initialization to use dynamic environment variables and enhance SQL migration policies for better clarity

--- src/lib/supabase-client.ts
+++ src/lib/supabase-client.ts
@@ -1,5 +1,8 @@
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
+import { env } from '$env/dynamic/public';
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './database.types';
 
-export const supabase = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
+export const supabase = createClient<Database>(
+	env.PUBLIC_SUPABASE_URL,
+	env.PUBLIC_SUPABASE_ANON_KEY
+);
--- supabase/migrations/20251102222127_add_bingo_presets.sql
+++ supabase/migrations/20251102222127_add_bingo_presets.sql
@@ -1,6 +1,8 @@
+create extension if not exists "uuid-ossp";
+
 create table
     presets (
-        id uuid primary key default uuid_generate_v4 (),
+        id uuid primary key default extensions.uuid_generate_v4 (),
         owner uuid not null references auth.users (id) on delete cascade,
         name text not null,
         created_at timestamp
@@ -13,7 +15,7 @@ create table
 
 create table
     preset_items (
-        id uuid primary key default uuid_generate_v4 (),
+        id uuid primary key default extensions.uuid_generate_v4 (),
         preset_id uuid not null references presets (id) on delete cascade,
         name text not null,
         description text
@@ -23,6 +25,10 @@ alter table presets enable row level security;
 
 alter table preset_items enable row level security;
 
-create policy "Allow access to all rows in presets" on presets using (true);
+create policy "Allow read access to all rows in presets" on presets for
+select
+    using (true);
 
-create policy "Allow access to all rows in preset_items" on preset_items using (true);
+create policy "Allow read access to all rows in preset_items" on preset_items for
+select
+    using (true);
