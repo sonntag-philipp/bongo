961ca0b1894fb61f093e81c97e3c2db0f5958a17
Flypsi <github@flyp.si>
2025-11-03 01:31:21 +0100

    feat: add GameToggle component with tooltip functionality and update page to use it

---
+++ src/lib/components/game-toggle.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { Toggle } from './ui/toggle';
+	import * as Tooltip from './ui/tooltip';
+
+	let { name, description } = $props();
+</script>
+
+<Tooltip.Provider>
+	<Tooltip.Root>
+		<Tooltip.Trigger>
+			<Toggle size="lg">
+				<div class="flex flex-col p-4">
+					<span class="text-xl">{name}</span>
+				</div>
+			</Toggle>
+		</Tooltip.Trigger>
+		<Tooltip.Content>
+			{description}
+		</Tooltip.Content>
+	</Tooltip.Root>
+</Tooltip.Provider>
--- src/routes/board/[boardId]/+page.svelte
+++ src/routes/board/[boardId]/+page.svelte
@@ -1,19 +1,9 @@
 <script>
+	import GameToggle from '$lib/components/game-toggle.svelte';
+
 	let { data } = $props();
 </script>
 
-<div class="container mx-auto p-4">
-	<ul class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-		{#each data.presetItems as items}
-			<li
-				class="flex h-48 flex-col items-center justify-center rounded-lg border border-gray-300 bg-gray-100 p-4 text-center"
-			>
-				<span class="text-xl font-bold">{items.name}</span>
-				<span class="text-sm text-gray-600">{items.description}</span>
-			</li>
-		{/each}
-	</ul>
+<div class="p-16">
+	<GameToggle description={data.presetItems[0].description} name={data.presetItems[0].name} />
 </div>
-
-<style>
-</style>
