e08e7dab3d81b77f9fd578c02b7e95ac5c4ea5ca
Flypsi <github@flyp.si>
2025-12-21 00:00:34 +0100

    feat(db): add new preset and preset items with descriptions
    - Remove auth.user seed data
    - Add new preset "Friendly Fire" with 20 associated items
    - Add description field to presets table
    - Update existing preset items with descriptions
    - Modify "Thermomix" item to "ggm gastro" with new description

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,88 +1,27 @@
 INSERT INTO
-    "auth"."users" (
-        "instance_id",
+    "public"."presets" (
         "id",
-        "aud",
-        "role",
-        "email",
-        "encrypted_password",
-        "email_confirmed_at",
-        "invited_at",
-        "confirmation_token",
-        "confirmation_sent_at",
-        "recovery_token",
-        "recovery_sent_at",
-        "email_change_token_new",
-        "email_change",
-        "email_change_sent_at",
-        "last_sign_in_at",
-        "raw_app_meta_data",
-        "raw_user_meta_data",
-        "is_super_admin",
+        "owner",
+        "name",
         "created_at",
-        "updated_at",
-        "phone",
-        "phone_confirmed_at",
-        "phone_change",
-        "phone_change_token",
-        "phone_change_sent_at",
-        "confirmed_at",
-        "email_change_token_current",
-        "email_change_confirm_status",
-        "banned_until",
-        "reauthentication_token",
-        "reauthentication_sent_at",
-        "is_sso_user",
         "deleted_at",
-        "is_anonymous"
+        "description"
     )
-VALUES
-    (
-        '00000000-0000-0000-0000-000000000000',
-        '3bc0bcba-0d33-4b1c-9109-afc6b20e03cb',
-        'authenticated',
-        'authenticated',
-        'test@flyp.si',
-        '$2a$10$elzbg1.qQhuHBJmns5L/Z.1/plQpzH0b3bEMfdsYI9sNtjGsoDGnS',
-        '2025-11-02 23:17:59.369053+00',
-        null,
-        '',
-        null,
-        '',
-        null,
-        '',
-        '',
-        null,
-        null,
-        '{"provider":"email","providers":["email"]}',
-        '{"email_verified":true}',
-        null,
-        '2025-11-02 23:17:59.363775+00',
-        '2025-11-02 23:17:59.369333+00',
-        null,
-        null,
-        '',
-        '',
-        null,
-        default,
-        '',
-        '0',
-        null,
-        '',
-        null,
-        'false',
-        null,
-        'false'
-    );
-
-INSERT INTO
-    "public"."presets" ("id", "owner", "name", "created_at", "deleted_at")
 VALUES
     (
         '31ba03c3-3ac8-4e51-adc7-deaf5d9a8846',
         '3bc0bcba-0d33-4b1c-9109-afc6b20e03cb',
         'Moodi Foodi',
         '2025-11-02 23:18:28.278999+00',
+        null,
+        null
+    ),
+    (
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        '3bc0bcba-0d33-4b1c-9109-afc6b20e03cb',
+        'Friendly Fire',
+        '2025-12-06 14:34:38+00',
+        null,
         null
     );
 
@@ -90,10 +29,65 @@ INSERT INTO
     "public"."preset_items" ("id", "preset_id", "name", "description")
 VALUES
     (
+        '03873435-143b-4822-8dd1-8139f9e494f7',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        'PhunkXGronkh-Moment',
+        null
+    ),
+    (
+        '08b25058-dd7a-4d1e-96d0-61b7b89de575',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        'Star-Gast',
+        null
+    ),
+    (
+        '1310c345-34ab-43ad-8019-68145a91c0e2',
+        '31ba03c3-3ac8-4e51-adc7-deaf5d9a8846',
+        'Maschine bester Comedian',
+        'Die Maschine macht lustige Geräusche oder sieht so aus als würde sie kotzen oder lustig'
+    ),
+    (
+        '1700e5ff-48b0-4453-be04-cf1a5550d0e0',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        '"ich will auch mitspielen"',
+        null
+    ),
+    (
+        '19e84f62-1ca7-4bc0-a702-e4fb92a42017',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        'Jay reißt unlustige Witze',
+        null
+    ),
+    (
+        '1d2cc781-d39e-4927-8dcc-728548206763',
+        '31ba03c3-3ac8-4e51-adc7-deaf5d9a8846',
+        'Ohne Fleischwolf geht nix',
+        'Ja muss halt alles erst durch Fleischwolf'
+    ),
+    (
         '1ff83e8d-f391-4be6-b4b7-75aa298abfa5',
         '31ba03c3-3ac8-4e51-adc7-deaf5d9a8846',
         'Mayonnaise à la truffe',
         'Es werden total fancy Saucen benutzt im Restaurant. Aber auch möglich wenn ein random Imbiss sich total toll findet, weil er einen fertigen Ketchup mit 3 TL Curry anreichert. (Hier bin ich mir nicht sicher lol)'
+    ),
+    (
+        '216cfd4e-7ca6-41d6-a4c9-124a59861128',
+        '31ba03c3-3ac8-4e51-adc7-deaf5d9a8846',
+        'Ehemaliger Fachinformatiker',
+        'Andere Ausbildung gemacht, aber dann doch keine Lust gehabt oder Burnout und Food Truck eröffnet'
+    ),
+    (
+        '2413e29b-27a1-47ea-829d-e5780fb67cf4',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        'Jemand wird als Frau geschminkt/Pan als Mann
+',
+        null
+    ),
+    (
+        '2904dcf8-060d-434d-a1bc-26f96f890d08',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        '"Wo ist xxx?"',
+        null
     ),
     (
         '3ca4a612-4e86-491b-abb8-e71cef908643',
@@ -108,10 +102,52 @@ VALUES
         'In das Essen wird extrem viel Butter bzw. Alternativen zu Butter gepackt.'
     ),
     (
+        '42b037e3-7ea9-45ce-b608-639492910a64',
+        '31ba03c3-3ac8-4e51-adc7-deaf5d9a8846',
+        'VDE Verbrechen',
+        'Energieversorgung sieht sehr unsicher aus, TÜV und VDE sehen das nicht so gerne'
+    ),
+    (
+        '44ac444c-3ab8-4f50-9009-0930fb154105',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        '"geiler Typ, ey"',
+        null
+    ),
+    (
+        '45cebda7-bed5-462a-8bec-0155481af2b8',
+        '31ba03c3-3ac8-4e51-adc7-deaf5d9a8846',
+        'Dekadente Preise',
+        'Preise einfach viel zu teuer für das Angebot'
+    ),
+    (
+        '4b301e10-8a71-4b32-a7b0-aa1d5fae3062',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        '"unfassbare Summe"',
+        null
+    ),
+    (
+        '5a77c779-6797-447a-9c88-f1cae96e779c',
+        '31ba03c3-3ac8-4e51-adc7-deaf5d9a8846',
+        'Praktikant ist auch dabei',
+        'Person arbeitet sehr langsam bzw. ein Praktikant ist dabei und muss noch eingearbeitet werden etc.'
+    ),
+    (
+        '5c4fa850-3dac-4f88-b6b4-4231812f6c88',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        '"Was machen wir jetzt?"',
+        null
+    ),
+    (
         '60a15ed9-77fc-48b6-8fbd-02b0e921b279',
         '31ba03c3-3ac8-4e51-adc7-deaf5d9a8846',
         'Stück Kohle',
         'Das Essen sieht so aus wie ein Stück Steinkohle.'
+    ),
+    (
+        '60a6b345-f54e-442a-b6bf-e0aa8d8c036a',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        'jemand trägt was weihnachtliches',
+        null
     ),
     (
         '6dae560c-1a89-49af-8599-42cccf57ecba',
@@ -126,6 +162,30 @@ VALUES
         'Die Preise oder Gerichte auf der Speisekarte wurden überklebt bzw. überschrieben weil niemand dran gedacht hat, dass sich die Preise oder das Angebot bald ändern könnten.'
     ),
     (
+        '80fa8c8e-ef5d-4b2a-8081-7862a0584123',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        '"Wahnsinn!"',
+        null
+    ),
+    (
+        '819ab85b-a461-4519-b935-f361049501f2',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        '"gleich gibts Essen"',
+        null
+    ),
+    (
+        '890ac209-a8cf-4b3e-805f-faefbf52dd3a',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        '2 um 12',
+        null
+    ),
+    (
+        '8cbf54f3-b5e5-4efd-beb9-c48bca556172',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        'es wird rasiert/gefärbt',
+        null
+    ),
+    (
         '943d1108-a678-490b-a7ad-fdc7d454e17b',
         '31ba03c3-3ac8-4e51-adc7-deaf5d9a8846',
         'Veggie ist doof',
@@ -138,16 +198,58 @@ VALUES
         'Die Besitzer oder Mitarbeiter spenden Obdachlosen ein Essen oder geben heiße Getränke etc. aus - oder verhalten sich generell sehr nett gegenüber Obdachlosen.'
     ),
     (
+        '9f346629-9d18-4ddc-8b0d-e92d3cb12176',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        'richtige Kamera suchen',
+        null
+    ),
+    (
+        'b405533f-0e76-4161-8de0-deb8770e9fb8',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        'Staff will nicht im bild sein',
+        null
+    ),
+    (
+        'b7ed0151-60a8-4a15-b104-af6563565331',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        '"Pan ist die einzige Frau"',
+        null
+    ),
+    (
         'ba782ed9-2947-4e0f-95d0-9c1413e1415c',
         '31ba03c3-3ac8-4e51-adc7-deaf5d9a8846',
         'Alles für die Fans',
         'Das Restaurant "lohnt sich nicht". Der Betreiber macht alles aus "Liebe" zu seinen Kunden und weil "es ihm einfach Spaß macht".'
     ),
     (
+        'baf684cd-7945-4080-b90a-8f2dbad58198',
+        '31ba03c3-3ac8-4e51-adc7-deaf5d9a8846',
+        'Frittierte Bratwurst',
+        'Die Fritteuse wird für alles verwendet bis auf die Getränke.'
+    ),
+    (
+        'c18486cc-79b1-44bd-8bfd-f394b7dd7f8b',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        'echt Küsse beim Karten-Küssen',
+        null
+    ),
+    (
         'c842272a-d17e-4f62-abce-e8947f226b69',
         '31ba03c3-3ac8-4e51-adc7-deaf5d9a8846',
         'Frau Schreiber kann nicht ruhig sein',
         'Eine Person in der Doku stellt sich selbst in den Mittelpunkt und nicht die Taten. Es werden irgendwelche Background Infos über Herrmann gegeben der vor 100 Jahren mal den Parkplatz vermietet hat auf dem der Cousin sein Auto gekauft hat.'
+    ),
+    (
+        'c8b817d4-0811-4b59-a65a-ed34297508e7',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        '"Ja, Mann!"',
+        null
+    ),
+    (
+        'cc70420d-2a36-4c51-b241-1a07515d631d',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        'langweiliges Spiel',
+        null
     ),
     (
         'cd5f651a-6ca0-4b75-bab8-69e354badb37',
@@ -162,6 +264,18 @@ VALUES
         'Das Restaurant benutzt keine Kassensysteme, akzeptiert kein EC oder schließt Kassensysteme nicht.'
     ),
     (
+        'de3c5dff-3e26-4d30-8071-f9e7ab728eae',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        'Fishkop macht Ausdruckstanz',
+        null
+    ),
+    (
+        'df0b4bd4-7125-4470-a657-a020da592b7e',
+        '31ba03c3-3ac8-4e51-adc7-deaf5d9a8846',
+        'Tüten und TK Krams',
+        'Egal was in diesem wunderbaren Etablissement verarbeitet wird, es kommt entweder aus Tüten oder irgendwelchen TK Packungen.'
+    ),
+    (
         'e1edab1d-fb91-4dcd-a71a-8302f952047b',
         '31ba03c3-3ac8-4e51-adc7-deaf5d9a8846',
         'Schaschlik Maschlik',
@@ -174,6 +288,30 @@ VALUES
         'Die Person trägt die ganze Zeit (schwarze) Handschuhe, auch beim Annehmen/Herausgeben von Bargeld.'
     ),
     (
+        'ed1968b2-cdd1-4649-bb1c-e9a3c972095a',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        '"das können wir nächstes Jahr nicht mehr toppen"',
+        null
+    ),
+    (
+        'f2042268-1dba-4597-a03b-4e92a6eae221',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        'keiner weiß wie ein Spiel funktioniert',
+        null
+    ),
+    (
+        'f4a1bc7c-242a-4e9e-8898-7886fc47e328',
+        '5058bb1c-82b8-42fd-8c1e-15e123be496d',
+        'Sponsor spendet',
+        null
+    ),
+    (
+        'f8e7f35e-4cdb-42ab-ab49-7322a5cff1f8',
+        '31ba03c3-3ac8-4e51-adc7-deaf5d9a8846',
+        'Kein fließend Wasser',
+        'Gibt halt keinen Wasseranschluss und auf dem Markt müssen Flaschen oder Kanister ausreichen'
+    ),
+    (
         'fbbaf55e-4a00-41cf-83d8-9ffa6543f660',
         '31ba03c3-3ac8-4e51-adc7-deaf5d9a8846',
         'Handarbeit sein Vadder',
@@ -182,6 +320,6 @@ VALUES
     (
         'fe515452-a7fe-4215-a39b-3dc80fddef01',
         '31ba03c3-3ac8-4e51-adc7-deaf5d9a8846',
-        'Thermomix',
-        'Es wird ein Thermomix in der Küche verwendet.'
+        'ggm gastro ',
+        'Die Einrichtung kommt vom IKEA für Restaurants'
     );
