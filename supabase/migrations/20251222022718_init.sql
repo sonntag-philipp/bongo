3ba9ea784892d639b973515425dadc277285fb59
Flypsi <github@flyp.si>
2025-12-22 04:01:24 +0100

    refactor(db): rename primary key constraints and add RLS policies
    - Update primary key constraint names for `board_presets` and `board_preset_items`
    - Add row level security to both tables
    - Create read access policies for public schema tables

--- supabase/migrations/20251222022718_init.sql
+++ supabase/migrations/20251222022718_init.sql
@@ -11,9 +11,9 @@ create table
             time zone null,
             slug text null,
             description text null,
-            constraint presets_pkey primary key (id),
+            constraint board_presets_pkey primary key (id),
             constraint board_presets_slug_key unique (slug),
-            constraint presets_owner_fkey foreign KEY (owner) references auth.users (id) on delete CASCADE
+            constraint board_presets_owner_fkey foreign KEY (owner) references auth.users (id) on delete CASCADE
     ) TABLESPACE pg_default;
 
 create table
@@ -22,6 +22,18 @@ create table
         preset_id uuid not null,
         name text not null,
         description text null,
-        constraint preset_items_pkey primary key (id),
-        constraint preset_items_preset_id_fkey foreign KEY (preset_id) references board_presets (id) on delete CASCADE
+        constraint board_preset_items_pkey primary key (id),
+        constraint board_preset_items_preset_id_fkey foreign KEY (preset_id) references board_presets (id) on delete CASCADE
     ) TABLESPACE pg_default;
+
+alter table public.board_presets enable row level security;
+
+alter table public.board_preset_items enable row level security;
+
+create policy "Allow read access to all rows in public.board_presets" on public.board_presets for
+select
+    using (true);
+
+create policy "Allow read access to all rows in public.board_preset_items" on public.board_preset_items for
+select
+    using (true);
